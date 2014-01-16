f768e0afe71c0db885a870d15992920f5f4f59e1
Jelte Fennema <jelte12345@gmail.com>
2014-01-16 02:39:20 +0100

    Update PyPi README

--- setup.py
+++ setup.py
@@ -20,19 +20,69 @@ Currently those are:
 Everything else you want you can still add to the document by adding LaTeX
 formatted strings.
 
-Support
-~~~~~~~
-
-This library has only been tested on Linux using Python 3.3. It might work
-fully or partially on any differently configured systems. I will not test with
-different configurations and I will not write fixes as well. You can of course
-always submit a pull request.
 
 Dependencies
 ~~~~~~~~~~~~
 
-- Python 3.3
-- LaTeX (only if you want to compile the tex file)
+- Python 3.3 (2.7 currently works as well)
+- pdflatex (only if you want to compile the tex file)
+
+
+Installation
+~~~~~~~~~~~~
+
+.. code::
+   pip install pylatex`
+
+Example
+~~~~~~~
+
+.. code:: python
+    from pylatex import Document, Section, Table
+    from pylatex.utils import italic
+
+    doc = Document()
+    section = Section('Yaay the first section, it can even be ' + italic('italic'))
+
+    table = Table('r|ccl')
+    table.add_hline()
+    table.add_row((1, 2, 3, 4))
+    table.add_hline(1, 2)
+    table.add_empty_row()
+    table.add_row((4, 5, 6, 7))
+
+    section.content.append(table)
+    doc.content.append(section)
+    doc.generate_pdf()
+
+This code will generate this:
+.. image:: https://raw.github.com/JelteF/PyLaTeX/master/docs/static/screenshot.png
+
+
+Future development
+~~~~~~~~~~~~~~~~~~
+
+I will keep adding functionality I need to this library, an interface for
+graphics and math will probably be added in a future version.
+
+If you add a feature yourself, or fix a bug, please send a pull request.
+
+You can submit issues, but it will not be my priority to fix them. My job and
+education are a bit higher on the priority list.
+
+
+Support
+~~~~~~~
+
+This library is being developed for Python 3.3. It currently works for Python
+2.7 as well, but further aditions to the library might break that
+compatibility. It is also only tested on Linux, so it might not work on any
+different platforms.
+
+I have no intention of testing on any different platforms or with different
+Python versions. I also don't have the intention to write fixes for platform or
+environment specific bugs, but pull requests that fix those are always welcome.
+
 
 """
 from distutils.core import setup
