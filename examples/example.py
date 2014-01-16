712501a659ffc76c33042c0c9f7a8f9609c1247c
Jelte Fennema <jelte12345@gmail.com>
2014-01-16 02:22:49 +0100

    Update example and readme

--- examples/example.py
+++ examples/example.py
@@ -4,15 +4,14 @@ from pylatex import Document, Section, Table
 from pylatex.utils import italic
 
 doc = Document()
-section = Section('Yaaaay the first section, it can even be ' +
-                  italic('italic'))
+section = Section('Yaay the first section, it can even be ' + italic('italic'))
 
-table = Table('r|cl')
+table = Table('r|ccl')
 table.add_hline()
-table.add_row((1, 2, 3))
+table.add_row((1, 2, 3, 4))
 table.add_hline(1, 2)
 table.add_empty_row()
-table.add_row((4, 5, 6))
+table.add_row((4, 5, 6, 7))
 
 section.content.append(table)
 doc.content.append(section)
