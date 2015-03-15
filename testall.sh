84d00fa7a54ee6bb38785009ae08514ad0e364fe
Jelte Fennema <github-tech@jeltef.nl>
2015-03-15 18:39:51 +0100

    The test script now uses the python2 converted scripts if the python version is 2.*

--- testall.sh
+++ testall.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-for f in {tests,examples}/*.py; do
+python_version=$(python --version |& sed 's|Python \(.\).*|\1|g')
+
+if [ "$python_version" = '2' ]; then
+    main_folder=python2_source
+else
+    main_folder=.
+fi
+
+for f in $main_folder/{tests,examples}/*.py; do
     echo -e '\e[32mTesting '$f'\e[0m'
     if ! python $f; then
         exit 1
