e5d831f2f976511d2bf991c2c7adb5a2c3b7ebaa
Sean McLemon <sean.mclemon@gmail.com>
2015-03-31 18:53:53 +0200

    initial commit for support simple list structure support in PyLaTex

---
+++ examples/lists_example.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Test for list structures in PyLaTeX. 
+# More info @ http://en.wikibooks.org/wiki/LaTeX/List_Structures
+
+from pylatex import Document, Section, Itemize, Enumerate, Description
+
+
+doc = Document()
+
+# create a bulleted "itemize" list like the below:
+# \begin{itemize}
+#   \item The first item
+#   \item The second item
+#   \item The third etc \ldots
+# \end{itemize}
+
+with doc.create(Section('"Itemize" list')):
+    with doc.create(Itemize()) as enum:
+        enum.add_item("the first item")
+        enum.add_item("the second item")
+        enum.add_item("the third etc \\ldots")
+
+# create a numbered "enumerate" list like the below:
+# \begin{enumerate}
+#   \item The first item
+#   \item The second item
+#   \item The third etc \ldots
+# \end{enumerate}
+
+with doc.create(Section('"Enumerate" list')):
+    with doc.create(Enumerate()) as enum:
+        enum.add_item("the first item")
+        enum.add_item("the second item")
+        enum.add_item("the third etc \\ldots")
+
+# create a labelled "description" list like the below:
+# \begin{description}
+#   \item[First] The first item
+#   \item[Second] The second item
+#   \item[Third] The third etc \ldots
+# \end{description}
+
+with doc.create(Section('"Description" list')):
+    with doc.create(Description()) as enum:
+        enum.add_item("First", "The first item")
+        enum.add_item("Second", "The second item")
+        enum.add_item("Third", "The third etc \\ldots")
+
+doc.generate_pdf()
+
---
+++ pylatex/lists.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+    pylatex.lists
+    ~~~~~~~
+
+    This module implements the class that deals with latex List objects, 
+    specifically Enumerate and Itemize.
+
+    :copyright: (c) 2015 by Sean McLemon.
+    :license: MIT, see License for more details.
+"""
+
+from .command import Command
+from .base_classes import BaseLaTeXNamedContainer
+
+
+def Item(s, label=None):
+    u"""Returns the string itemized.
+
+        :param s:
+
+        :type s: str
+
+        :return:
+        :rtype: str
+    """
+    if label:
+        return r'\item['+ label + ']{' + s + '}'
+
+    return r'\item{' + s + '}'
+
+class List(BaseLaTeXNamedContainer):
+
+    """A class that represents a list."""
+
+    def __init__(self, list_spec=None, data=None, pos=None, list_type="enumerate",
+                 **kwargs):
+        """
+            :param list_spec:
+            :param list_type:
+            :param data:
+            :param pos:
+
+            :type list_spec: str
+            :type list_type: str
+            :type data: list
+            :type pos: list
+        """
+        super().__init__(list_type, data=data, options=pos,
+                         argument=list_spec, **kwargs)
+
+    def add_item(self, item):
+        """ Adds an item to the list.
+
+            :param item:
+
+            :type item: string
+        """
+        self.append(Item(item))
+
+
+class Enumerate(List):
+
+    """ A class that represents an enumerate list """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, list_type='enumerate', **kwargs)
+
+
+class Itemize(List):
+
+    """ A class that represents an itemize list """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, list_type='itemize', **kwargs)
+
+
+class Description(List):
+
+    """ A class that represents a description list """
+
+    def __init__(self, *args, **kwargs):
+        super(Description, self).__init__(*args, list_type='description', **kwargs)
+
+    def add_item(self, label, item):
+        """ Adds an item to the list.
+
+            :param label:
+            :param item:
+
+            :type label: string
+            :type item: string
+        """
+        self.append(Item(item, label))
