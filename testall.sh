6c1695e06b39819ba6a6e85221ac17862eaa26c2
votti <mcvotti@gmail.com>
2015-05-14 12:59:05 +0200

    Added an optional, named command line argument -p COMMAND to testall.sh to be able to provide a python command to be used.

--- testall.sh
+++ testall.sh
@@ -1,4 +1,31 @@
 #!/usr/bin/env bash
+# This script runs flake8 to test for pep8 compliance and executes all the examples and tests
+# run as: testall.sh [-p COMMAND] [clean]
+# Optional positional arguments
+#       clean: cleans up the latex files generated
+# Optional named arguments:
+#      -p COMMAND: the python command that should be used, e.g. ./testall.sh -p python3
+#
+
+# Default values
+python="python"
+
+# Check if a command line argument was provided as an input argument.
+while getopts ":p:" opt; do
+  case $opt in
+    p)
+      python=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
 # Check code guidelines
 echo -e '\e[32mChecking for code style errors \e[0m'
@@ -6,7 +33,8 @@ if ! flake8 pylatex examples tests; then
     exit 1
 fi
 
-python_version=$(python --version |& sed 's|Python \(.\).*|\1|g')
+# Run the examples and tests
+python_version=$($python --version |& sed 's|Python \(.\).*|\1|g')
 
 if [ "$python_version" = '2' ]; then
     main_folder=python2_source
@@ -16,11 +44,11 @@ fi
 
 for f in $main_folder/{tests,examples}/*.py; do
     echo -e '\e[32mTesting '$f'\e[0m'
-    if ! python $f; then
+    if ! $python $f; then
         exit 1
     fi
 done
 
-if [ "$1" = 'clean' ]; then
+if [ "${@:$OPTIND:1}" = 'clean' ]; then
     rm *.pdf *.log *.aux *.tex
 fi
