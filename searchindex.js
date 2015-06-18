ee9f097c834f9d565f9c9198411645ee2a8abfb2
Jelte Fennema <github-tech@jeltef.nl>
2015-06-18 15:51:45 +0200

    Initial Sphinx documentation

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to PyLaTeX&#8217;s documentation!"],titleterms:{document:0,indic:0,pylatex:0,tabl:0,welcom:0}})
