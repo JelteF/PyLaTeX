8d7f1570592e020047a255e8ff0cd9a8fd1f2b9e
Jelte Fennema <github-tech@jeltef.nl>
2015-06-22 19:51:54 +0200

    Add quantities to documentation

--- examples/quantities_ex.py
+++ examples/quantities_ex.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python
+"""
+    Quantities example
+    ~~~~~~~~~~~~~~~~~~
 
+    This example shows quantities functionality.
+
+    .. literalinclude:: /../../examples/quantities_ex.py
+        :start-after: begin-doc-include
+        :lines: 10-
+
+    ..  :copyright: (c) 2014 by Jelte Fennema.
+        :license: MIT, see License for more details.
+"""
+
+# begin-doc-include
 import quantities as pq
 
 from pylatex import Document, Section, Subsection, Math
 from pylatex.quantities import Quantity
 
-doc = Document()
-section = Section('Quantity tests')
-subsection = Subsection('Scalars')
+if __name__ == '__main__':
+    doc = Document()
+    section = Section('Quantity tests')
+    subsection = Subsection('Scalars')
 
-G = pq.constants.Newtonian_constant_of_gravitation
-moon_earth_distance = 384400*pq.km
-moon_mass = 7.34767309e22 * pq.kg
-earth_mass = 5.972e24 * pq.kg
-moon_earth_force = G*moon_mass*earth_mass/moon_earth_distance**2
-q1 = Quantity(moon_earth_force.rescale(pq.newton))
-math = Math(data=['F=', q1])
-subsection.append(math)
-section.append(subsection)
-doc.append(section)
-doc.generate_pdf()
+    G = pq.constants.Newtonian_constant_of_gravitation
+    moon_earth_distance = 384400*pq.km
+    moon_mass = 7.34767309e22 * pq.kg
+    earth_mass = 5.972e24 * pq.kg
+    moon_earth_force = G*moon_mass*earth_mass/moon_earth_distance**2
+    q1 = Quantity(moon_earth_force.rescale(pq.newton))
+    math = Math(data=['F=', q1])
+    subsection.append(math)
+    section.append(subsection)
+    doc.append(section)
+    doc.generate_pdf()
