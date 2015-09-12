65998b5a2683e2bada07cc1588aef53d054f70ea
Jelte Fennema <github-tech@jeltef.nl>
2015-09-12 19:49:55 +0200

    Make the new setup.py work with pip as well

--- setup.py
+++ setup.py
@@ -156,6 +156,7 @@ Copyright 2014 Jelte Fennema, under `the MIT license
 try:
     from setuptools import setup
     from setuptools.command.install import install
+    from setuptools.command.egg_info import egg_info
 except ImportError:
     from distutils.core import setup
 import sys
@@ -185,6 +186,8 @@ if sys.version_info[0] == 3:
 else:
     source_dir = 'python2_source'
 
+PY2_CONVERTED = False
+
 
 extras['all'] = list(set([req for reqs in extras.values() for req in reqs]))
 
@@ -192,25 +195,36 @@ extras['all'] = list(set([req for reqs in extras.values() for req in reqs]))
 # Automatically convert the source from Python 3 to Python 2 if we need to.
 class CustomInstall(install):
     def run(self):
-        if source_dir == 'python2_source':
-            try:
-                # Check if 3to2 exists
-                subprocess.check_output(['3to2', '--help'])
-            except OSError as e:
-                if e.errno != errno.ENOENT:
-                    raise e
-                if not os.path.exists(os.path.join(source_dir, 'pylatex')):
-                    raise ImportError('3to2 needs to be installed before '
-                                      'installing when PyLaTeX for Python 2.7 '
-                                      'when it is not installed using one of '
-                                      'the pip releases.')
-            else:
-                converter = os.path.dirname(os.path.realpath(__file__)) \
-                    + '/convert_to_py2.sh'
-                print(converter)
-                subprocess.check_call([converter])
-
+        convert_to_py2()
         install.run(self)
+
+
+class CustomEggInfo(egg_info):
+    def initialize_options(self):
+        convert_to_py2()
+        egg_info.initialize_options(self)
+
+
+def convert_to_py2():
+    if source_dir == 'python2_source' and not PY2_CONVERTED:
+        try:
+            # Check if 3to2 exists
+            subprocess.check_output(['3to2', '--help'])
+        except OSError as e:
+            if e.errno != errno.ENOENT:
+                raise e
+            if not os.path.exists(os.path.join(source_dir, 'pylatex')):
+                raise ImportError('3to2 needs to be installed before '
+                                  'installing when PyLaTeX for Python 2.7 '
+                                  'when it is not installed using one of '
+                                  'the pip releases.')
+        else:
+            converter = os.path.dirname(os.path.realpath(__file__)) \
+                + '/convert_to_py2.sh'
+            subprocess.check_call([converter])
+            global PY2_CONVERTED
+            PY2_CONVERTED = True
+
 
 setup(name='PyLaTeX',
       version='0.8.0',
@@ -224,7 +238,10 @@ setup(name='PyLaTeX',
       license='MIT',
       install_requires=['ordered-set'],
       extras_require=extras,
-      cmdclass={'install': CustomInstall},
+      cmdclass={
+          'install': CustomInstall,
+          'egg_info': CustomEggInfo,
+      },
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
