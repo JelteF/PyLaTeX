30c4e88c3ff3a41fe254202dded58224762e352f
Björn Dahlgren <bjodah@gmail.com>
2016-01-21 12:32:29 +0100

    Add support for options in Quantity

--- pylatex/quantities.py
+++ pylatex/quantities.py
@@ -10,7 +10,7 @@ It requires the latex package SIunitx.
 
 from operator import itemgetter
 
-from .base_classes import Command
+from .base_classes import Command, Options
 from .package import Package
 from .utils import NoEscape
 
@@ -38,23 +38,39 @@ class Quantity(Command):
 
     packages = [Package('siunitx')]
 
-    def __init__(self, quantity, *, format_cb=None):
-        """
+    def __init__(self, quantity, options=None, *, format_cb=None):
+        r"""
         Args
         ----
         quantity: `quantities.quantity.Quantity`
             The quantity that should be displayed
-        fmtstr: callable
+        options: None, str, list or `~.Options`
+            Options of the command. These are placed in front of the arguments.
+        format_cb: callable
             A function which formats the number in the quantity. By default
             this uses `numpy.array_str`.
+
+        Examples
+        --------
+        >>> import quantities as pq
+        >>> speed = 3.14159265 * pq.meter / pq.second
+        >>> Quantity(speed, options={'round-precision': 3,
+        ...                          'round-mode': 'figures'})
+        '\\SI[round-mode=figures,round-precision=3]{3.14159265}{\meter\per\second}'
+
         """
         import numpy as np
 
         self.quantity = quantity
         self._format_cb = format_cb
+
         if format_cb is None:
             magnitude_str = np.array_str(quantity.magnitude)
         else:
             magnitude_str = format_cb(quantity.magnitude)
         unit_str = _dimensionality_to_siunitx(quantity.dimensionality)
-        super().__init__(command='SI', arguments=(magnitude_str, unit_str))
+        if options is not None:
+            options = Options(options)
+            options._escape = False  # siunitx uses dashes in kwargs
+        super().__init__(command='SI', arguments=(magnitude_str, unit_str),
+                         options=options)
--- tests/test_quantities.py
+++ tests/test_quantities.py
@@ -13,6 +13,10 @@ def test_quantity():
     q2 = Quantity(v, format_cb=lambda x: str(int(x)))
     assert q2.dumps() == r'\SI{1}{\meter\per\second}'
 
+    q3 = Quantity(v, options={'zero-decimal-to-integer': 'true'})
+    ref = r'\SI[zero-decimal-to-integer=true]{1.0}{\meter\per\second}'
+    assert q3.dumps() == ref
+
 
 def test_dimensionality_to_siunitx():
     assert _dimensionality_to_siunitx((pq.volt/pq.kelvin).dimensionality) == \
