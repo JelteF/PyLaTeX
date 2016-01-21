9e198c3a48b1a6b1025f945b1c04bff57a48e47c
Björn Dahlgren <bjodah@gmail.com>
2016-01-21 12:32:29 +0100

    Add support for options in Quantity

--- examples/quantities_ex.py
+++ examples/quantities_ex.py
@@ -21,7 +21,8 @@ if __name__ == '__main__':
     moon_mass = 7.34767309e22 * pq.kg
     earth_mass = 5.972e24 * pq.kg
     moon_earth_force = G * moon_mass * earth_mass / moon_earth_distance**2
-    q1 = Quantity(moon_earth_force.rescale(pq.newton))
+    q1 = Quantity(moon_earth_force.rescale(pq.newton),
+                  options={'round-precision': 4, 'round-mode': 'figures'})
     math = Math(data=['F=', q1])
     subsection.append(math)
     section.append(subsection)
--- tests/test_quantities.py
+++ tests/test_quantities.py
@@ -13,6 +13,9 @@ def test_quantity():
     q2 = Quantity(v, format_cb=lambda x: str(int(x)))
     assert q2.dumps() == r'\SI{1}{\meter\per\second}'
 
+    q3 = Quantity(v, options={'zero-decimal-to-integer': 'oink'})
+    assert q3.dumps() == r'\SI[zero-decimal-to-integer=oink]{1.0}{\meter\per\second}'
+
 
 def test_dimensionality_to_siunitx():
     assert _dimensionality_to_siunitx((pq.volt/pq.kelvin).dimensionality) == \
