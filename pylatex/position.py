1f9730bc943e32c6902001308b77003863ff23a2
Vladimir Gorovikov <vladimirg@versabank.com>
2016-06-01 16:51:48 -0600

    Added missing files (position.py)

---
+++ pylatex/position.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from .base_classes import Environment
+from .package import Package
+from .utils import NoEscape
+
+
+class Position(Environment):
+    r""" Base class for positioning environments
+    """
+    packages = [Package('ragged2e')]
+
+
+class Center(Position):
+    r""" Centered environment """
+
+
+class Flushleft(Position):
+    r""" Left-aligned environment """
+
+
+class Flushright(Position):
+    r""" Right-aligned environment """
+
+
+class Minipage(Environment):
+    r""" A class that allows the creation of minipages within document pages """
+    
+    def __init__(self, width=1):
+        r""" Instantiates a minipage within the current environment
+
+            width: float
+                width with respect to the text width of the page
+        """
+
+        arguments = [ NoEscape(str(width) + r'\textwidth') ]
+
+        super().__init__(arguments = arguments)
