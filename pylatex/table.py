698ad1017324c856845e428f73abe91f7ed0ef19
Vladimir Gorovikov <vsg235@mail.usask.ca>
2016-08-26 15:52:13 +0000

    Added xcolor package to colored MultiRows and MultiColumns

--- pylatex/table.py
+++ pylatex/table.py
@@ -247,6 +247,7 @@ class MultiColumn(Container):
 
         # Add a cell color to the MultiColumn
         if color is not None:
+            self.packages.append(Package('xcolor', options='table'))
             color_command = Command("cellcolor", arguments=color)
             self.append(color_command)
 
@@ -292,6 +293,7 @@ class MultiRow(Container):
         super().__init__(data=data)
 
         if color is not None:
+            self.packages.append(Package('xcolor', options='table'))
             color_command = Command("cellcolor", arguments=color)
             self.append(color_command)
 
