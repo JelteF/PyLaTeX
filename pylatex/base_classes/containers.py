7d34630eff6908008019d01a897c52a757ce2a8a
d.grigonis <d.grigonis@me.com>
2017-01-11 22:39:35 +0000

    indenting docstring fix

--- pylatex/base_classes/containers.py
+++ pylatex/base_classes/containers.py
@@ -125,8 +125,8 @@ class Environment(Container):
     The text that is used in the place of environment_name is by default the
     name of the class in lowercase.
     However, this default can be overridden in 2 ways:
-        1. setting the _latex_name class variable when declaring the class
-        2. setting the _latex_name attribute when initialising object
+    1. setting the _latex_name class variable when declaring the class
+    2. setting the _latex_name attribute when initialising object
     """
 
     #: Set to true if this full container should be equivalent to an empty
