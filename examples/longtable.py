5d6523eb531c0d1b6cd98fcf8f5f892ded115d92
JarrahGos <Jarrah.Gosbell@gmail.com>
2017-06-14 08:13:23 +1000

    Add longtable example

---
+++ examples/longtable.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+"""
+This example shows the functionality of the longtable element.
+
+It creates a sample multi-page spanning table
+
+..  :copyright: (c) 2017 by Jarrah Gosbell
+    :license: MIT, see License for more details.
+"""
+
+# begin-doc-include
+from pylatex import Document, LongTable, MultiColumn
+
+
+def genenerate_longtabu():
+    geometry_options = {
+        "margin": "2.54cm",
+        "includeheadfoot": True
+    }
+    doc = Document(page_numbers=True, geometry_options=geometry_options)
+
+    # Generate data table
+    with doc.create(LongTable("l l l")) as data_table:
+            data_table.add_hline()
+            data_table.add_row(["header 1", "header 2", "header 3"])
+            data_table.add_hline()
+            data_table.end_head()
+            data_table.add_hline()
+            data_table.add_row((MultiColumn(3, align='r',
+                                data='Containued on Next Page'),))
+            data_table.add_hline()
+            data_table.end_foot()
+            data_table.add_hline()
+            data_table.add_row((MultiColumn(3, align='r',
+                                data='Not Containued on Next Page'),))
+            data_table.add_hline()
+            data_table.end_last_foot()
+            row = ["Content1", "9", "Longer String"]
+            for i in range(150):
+                data_table.add_row(row)
+
+    doc.generate_pdf("longtable", clean_tex=False)
+
+genenerate_longtabu()
