e6697ed389571a607174e75aa8a95460406de181
PlusMinus0 <brandt.matthias+github@gmail.com>
2017-09-26 11:48:58 +0200

    Use spaces instead fo tabs.

--- tests/test_inheritance.py
+++ tests/test_inheritance.py
@@ -5,11 +5,11 @@ from pylatex import Document
 
 class TestInheritance(unittest.TestCase):
 
-	def test_latex_name(self):
-		class MyDoc(Document):
-			def __init__(self):
-				super().__init__()
+    def test_latex_name(self):
+        class MyDoc(Document):
+            def __init__(self):
+                super().__init__()
 
-		doc = Document()
-		my_doc = MyDoc()
-		self.assertEqual(my_doc.latex_name, doc.latex_name)
+        doc = Document()
+        my_doc = MyDoc()
+        self.assertEqual(my_doc.latex_name, doc.latex_name)
