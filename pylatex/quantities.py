7ffec0e38848753f5d86d9375c55dc1bc1a2be21
Yage Hu <yagejhu@gmail.com>
2018-01-31 19:35:53 -0800

    Fix uncertain quantities to work with siunitx

--- pylatex/quantities.py
+++ pylatex/quantities.py
@@ -82,7 +82,7 @@ class Quantity(Command):
             The quantity that should be displayed
         options: None, str, list or `~.Options`
             Options of the command. These are placed in front of the arguments.
-        format_cb: callableS
+        format_cb: callable
             A function which formats the number in the quantity. By default
             this uses `numpy.array_str`.
 
