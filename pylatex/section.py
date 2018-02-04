cd15a4262853f90967c90956d3cddd3dc7244a3a
Wddysr <Wddysr@users.noreply.github.com>
2018-02-04 13:08:26 +0100

    Adding label/ref support and use label in section.py (#182)
    * labelref.py/__init__.py/test_args.py: Add label/ref support
    
    This commit add the support for label and multiple types of ref.
    
    The new commands use a Marker object wich is the representation of
    the label/ref parameter in LaTeX.
    
    * section.py: Add label support to sectioning classes
    
    Add support for label in a way that let user choose between :
     - Specify his own Label
     - Automatic label (default)
     - No label
    
    This way sections can be referenced using the marker in their label object.
    
    * labelref.py: Add cref/Set remove_invalid_char as private
    
    Add support for cref and Cref commands.
    Set remove_invalid_char as private by adding _ before it.
    
    * section.py: make label keyword only / accept str
    
    Set the label parameter to keyword only.
    Add support for label as string in constructor.

--- pylatex/section.py
+++ pylatex/section.py
@@ -8,6 +8,7 @@ This module implements the section type classes.
 
 
 from .base_classes import Container, Command
+from .labelref import Marker, Label
 
 
 class Section(Container):
@@ -16,12 +17,15 @@ class Section(Container):
     #: A section should normally start in its own paragraph
     end_paragraph = True
 
+    #: Default prefix to use with Marker
+    marker_prefix = "sec"
+
     #: Number the sections when the section element is compatible,
     #: by changing the `~.Section` class default all
     #: subclasses will also have the new default.
     numbering = True
 
-    def __init__(self, title, numbering=None, **kwargs):
+    def __init__(self, title, numbering=None, *, label=True, **kwargs):
         """
         Args
         ----
@@ -29,12 +33,27 @@ class Section(Container):
             The section title.
         numbering: bool
             Add a number before the section title.
+        label: Label or bool or str
+            Can set a label manually or use a boolean to set
+            preference between automatic or no label
         """
 
         self.title = title
 
         if numbering is not None:
             self.numbering = numbering
+        if isinstance(label, Label):
+            self.label = label
+        elif isinstance(label, str):
+            if ':' in label:
+                label = label.split(':', 1)
+                self.label = Label(Marker(label[1], label[0]))
+            else:
+                self.label = Label(Marker(label, self.marker_prefix))
+        elif label:
+            self.label = Label(Marker(title, self.marker_prefix))
+        else:
+            self.label = None
 
         super().__init__(**kwargs)
 
@@ -53,6 +72,8 @@ class Section(Container):
             num = ''
 
         string = Command(self.latex_name + num, self.title).dumps()
+        if self.label is not None:
+            string += '\n' + self.label.dumps()
         string += '%\n' + self.dumps_content()
 
         return string
@@ -61,22 +82,34 @@ class Section(Container):
 class Part(Section):
     """A class that represents a part."""
 
+    marker_prefix = "part"
+
 
 class Chapter(Section):
     """A class that represents a chapter."""
+
+    marker_prefix = "chap"
 
 
 class Subsection(Section):
     """A class that represents a subsection."""
 
+    marker_prefix = "subsec"
+
 
 class Subsubsection(Section):
     """A class that represents a subsubsection."""
+
+    marker_prefix = "ssubsec"
 
 
 class Paragraph(Section):
     """A class that represents a paragraph."""
 
+    marker_prefix = "para"
+
 
 class Subparagraph(Section):
     """A class that represents a subparagraph."""
+
+    marker_prefix = "subpara"
