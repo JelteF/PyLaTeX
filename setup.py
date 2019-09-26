6275e2722f3a9db52bc9ae6029000010798231d4
Jelte Fennema <github-tech@jeltef.nl>
2019-09-26 08:32:43 +0200

    Hopefully fix ci

--- setup.py
+++ setup.py
@@ -27,9 +27,9 @@ extras = {
     'matrices': ['numpy'],
     'matplotlib': ['matplotlib'],
     'quantities': ['quantities', 'numpy'],
-    'testing': ['flake8<3.0.0', 'pep8-naming', 'flake8_docstrings==1.3.0',
-                'pycodestyle==3.0.0', 'pyflakes==1.2.3', 'nose',
-                'flake8-putty', 'coverage'],
+    'testing': ['flake8<3.0.0', 'pep8-naming==0.8.2',
+                'flake8_docstrings==1.3.0', 'pycodestyle==3.0.0',
+                'pyflakes==1.2.3', 'nose', 'flake8-putty', 'coverage'],
     'convert_to_py2': ['3to2', 'future>=0.15.2'],
 }
 
