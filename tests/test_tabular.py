0cca69d51e4a80d90871418f91eb03d2b493c5f5
neoglez <neoglez@gmail.com>
2023-07-17 14:27:03 +0200

    Update test_tabular.py

--- tests/test_tabular.py
+++ tests/test_tabular.py
@@ -18,13 +18,15 @@ def test_tabular_can_add_row_passing_many_arguments(sample_logo_path):
 
     with doc.create(Section('Can Add Row Passing Many Arguments')):
         with doc.create(Tabular("|c|c|", booktabs=True)) as table:                
+            
             mc1 = MultiColumn(1, align='l', data=StandAloneGraphic(
-                          filename=sample_logo_path)),
+                          filename=sample_logo_path))
             mc2 = MultiColumn(1, align='l', data=StandAloneGraphic(
                           filename=sample_logo_path))
 
                       
             table.add_row(mc1, mc2)
+    doc.generate_pdf(clean_tex=False)
         
 
 def test_tabular_can_add_row_passing_iterable(sample_logo_path):
@@ -51,12 +53,14 @@ def test_tabular_can_add_row_passing_iterable(sample_logo_path):
                  ]
                       
                 table.add_row(multi_columns_array)
+    doc.generate_pdf()
 
 if __name__ == '__main__':
     
-    import os.path as osp
+    import os.path as osp    
     
-    sample_logo_path = osp.join("..","examples", "sample-logo.png") 
+    sample_logo_path = osp.abspath(osp.join(
+        __file__[0:-15], "..", "examples", "sample-logo.png"))
     
     test_tabular_can_add_row_passing_many_arguments(
         sample_logo_path=sample_logo_path)
