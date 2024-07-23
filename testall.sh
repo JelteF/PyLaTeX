553032d4b75b8dd106cc068bc4756c944097c6e9
Jelte Fennema-Nio <github-tech@jeltef.nl>
2024-07-23 23:53:32 +0200

    Disable doc building on 3.7 and 3.8
    Some warning due to type hints

--- testall.sh
+++ testall.sh
@@ -92,7 +92,7 @@ if [ "$clean" = 'TRUE' ]; then
     rm -- *.pdf *.log *.aux *.tex *.fls *.fdb_latexmk >/dev/null
 fi
 
-if [[ "$nodoc" != 'TRUE' && "$python_version" == "3" && "$python_version_long" != 3.3.* && "$python_version_long" != 3.4.* && "$python_version_long" != 3.12.* ]]; then
+if [[ "$nodoc" != 'TRUE' && "$python_version" == "3" && "$python_version_long" != 3.7.* && "$python_version_long" != 3.8.* && "$python_version_long" != 3.12.* ]]; then
     echo -e '\e[32mChecking for errors in docs and docstrings\e[0m'
     cd docs
     set -e
